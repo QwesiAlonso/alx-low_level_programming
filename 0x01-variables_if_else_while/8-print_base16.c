68789e4bfce27c2b8fda34f9a588ea31641459c9
QwesiAlonso <sammyalonso4@gmail.com>
2023-06-16 22:25:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ * Return: Outcome 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 48; m <= 57; m++)
+	{
+		putchar(m);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
