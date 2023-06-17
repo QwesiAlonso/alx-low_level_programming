d89ef85f376b7344f1fca3d8460bf95cbda8220e
QwesiAlonso <sammyalonso4@gmail.com>
2023-06-16 21:47:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ * Return: Outcome 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		putchar(m + 48);
+	}
+	putchar('\n');
+	return (0);
+}
