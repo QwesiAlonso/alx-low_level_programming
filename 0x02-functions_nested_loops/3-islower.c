7d9bdaff659871e9c6af5c89a48d13372206ed30
QwesiAlonso <sammyalonso4@gmail.com>
2023-06-21 22:02:13 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _slower - Checks for learning character
+ * _islower - Checks for learning character
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
